54050c72f222e8f5b04acb0d792530065b7adffa
c0pson <pc312199@student.polsl.pl>
2025-11-19 22:23:36 +0100

    routes

--- spam_detection/api/routes_spam.py
+++ spam_detection/api/routes_spam.py
@@ -1,2 +1,17 @@
 """Flask web app 
 """
+from flask import Blueprint, render_template
+from flask import Flask
+
+print(__file__)
+
+spam_bp = Blueprint(
+    "spam",
+    __name__,
+    url_prefix="/api/spam",
+    
+)
+
+@spam_bp.route("/form")
+def show_form():
+    return render_template("index.html")
