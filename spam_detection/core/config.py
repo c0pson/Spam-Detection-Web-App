b78e29fdd98e059d68983be6605b836cfb3c93a7
c0pson <pc312199@student.polsl.pl>
2025-11-23 23:02:48 +0100

    refactor: defined SERVER enum for API configuration

--- spam_detection/core/config.py
+++ spam_detection/core/config.py
@@ -1,2 +1,10 @@
-"""URLs, API keys...
-"""
+from enum import Enum
+
+class SERVER(Enum):
+    URL = "http://79.76.44.20"
+    CHECK_SPAM = "/check_spam"
+    CHECK_AVAILABILITY = "/check_availability"
+    AVAILABLE = 200
+
+    def __add__(self, other):
+        return self.value + other.value
