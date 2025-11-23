e487e103728b12879b2d47df5805888f9072fdc4
c0pson <pc312199@student.polsl.pl>
2025-11-23 23:02:19 +0100

    implement SpamService class for spam classification using api

--- spam_detection/services/spam_checker.py
+++ spam_detection/services/spam_checker.py
@@ -1,2 +1,10 @@
-"""Communication with trained model. Connection handling with server (API)
-"""
+from spam_detection.utils.http import ServerCommunication
+
+from spam_detection.models.spam_request import SpamRequest
+
+class SpamService:
+    @staticmethod
+    def classify_text(text: str) -> bool:
+        payload = SpamRequest(text=text)
+        response = ServerCommunication.check_spam(payload)
+        return response.get("result") == "Spam"
