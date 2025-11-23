937b57df73ae124fa27f80c5b6f95835b2c5a041
c0pson <pc312199@student.polsl.pl>
2025-11-23 23:03:08 +0100

    fix: add availability check before rendering home page

--- spam_detection/main.py
+++ spam_detection/main.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
 from spam_detection.api.routes_spam import spam_bp
 
+from spam_detection.utils.http import ServerCommunication
+
 def create_app():
     app = Flask(__name__)
 
     @app.route("/")
     def home():
-        return render_template("home.html")
-
+        if ServerCommunication.check_availability():
+            return render_template("home.html")
+        else:
+            return render_template("404.html")
     app.register_blueprint(spam_bp)
-
     return app
 
 if __name__ == "__main__":
